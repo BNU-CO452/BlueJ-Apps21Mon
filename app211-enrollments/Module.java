3b07f99eceaf722392ff85ac82147fce4ee3f4fc
Derek Peacock <derek.peacock@yahoo.com>
2021-10-11 19:49:52 +0100

    Connected course to module

--- app211-enrollments/Module.java
+++ app211-enrollments/Module.java
@@ -1,44 +1,56 @@
 
 /**
- * This class models a BNU module which forms part
- * of a course.
+ * This class represents a course module and contains
+ * information on the module title and code.
  *
  * @author Derek Peacock
- * @version 1.0 
+ * @version 1.0 11/10/2021
  */
 public class Module
 {
-    // Variables
+    // instance variables - replace the example below with your own
+    
     private String code;
+    
     private String title;
-
+    
+    private int credit;
+    
     /**
      * Constructor for objects of class Module
      */
-    public Module()
+    public Module(String code, String title)
     {
+        // initialise instance variables
+        credit = 15;
+        this.code = code;
+        this.title = title;
     }
 
-     /**
-      * This method will printout the Module code and
-      * title
-      */
-    public void print()
+    public String getCode()
     {
-        printHeading();
-        
-        System.out.println(" Module Code: " + code + ": " + title);
-        System.out.println();
+        return code;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    } 
+    
+    public void setCredit(int credit)
+    {
+        this.credit = credit;
     }
     
     /**
-     * Print out the details of the course to the terminal.
+     * Print the details of the course, the list
+     * of students enrolled and the module
      */
-    private void printHeading()
+    public void print()
     {
-        System.out.println(" --------------------------------");
-        System.out.println("   App211: Module Details");
-        System.out.println(" --------------------------------");
+        System.out.println(" Module Code: " + code + ": " 
+                            + title + " Credit " + credit);
         System.out.println();
-    }    
+    }
+    
 }
