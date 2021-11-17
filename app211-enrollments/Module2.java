02dc9f7aceb97d54bd289b962d429585846d7806
Derek Peacock <derek.peacock@yahoo.com>
2021-11-17 18:27:44 +0000

    Added IntelliJ Project

---
+++ app211-enrollments/Module2.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Module2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module2
+{
+    private String code;
+
+    /**
+     * Constructor for objects of class Module2
+     */
+    public Module2(String code)
+    {
+        this.code = code;
+    }
+
+}
